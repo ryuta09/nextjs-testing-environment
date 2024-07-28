539c6e27ac2c064a57213a491da6c3218687eb60
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-07-28 13:31:55 +0900

    feat: TailwindVariants用のルーティング設定

---
+++ app/tailwindvariants/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TailwindVariants = () => {
+  return (
+    <div className='max-w-[1000px] w-full mx-auto my-5'>
+      <div>TailwindVariants</div>
+    </div>
+  )
+}
+
+export default TailwindVariants
