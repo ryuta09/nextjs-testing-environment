413321392d5a349b8e2834db1ad88b3b39eba260
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-08-24 21:31:23 +0900

    feat: ストアから呼び出し

--- app/zustand/page.tsx
+++ app/zustand/page.tsx
@@ -1,6 +1,13 @@
+import useCounterStore from "../stores/store"
+
 const Zustand = () => {
+  const {count, increment, decrement } = useCounterStore()
   return (
-    <div>zustand</div>
+    <>
+      <h2>count</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </>
   )
 }
 
