50bd4e998118644ecf18c551792e6fb0a8f0872d
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-10-10 23:34:36 +0900

    feat: useFormStateを使った実装例

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,5 @@
 "use server";
-export const formAction = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log("user data added to db");
+export const formAction = async (previousState: number) => {
+  console.log(previousState)
+  return previousState + 1
 };
--- app/rsc/page.tsx
+++ app/rsc/page.tsx
@@ -1,18 +1,18 @@
-'use client'
-import { useFormStatus } from "react-dom";
+"use client";
+import { useFormState, useFormStatus } from "react-dom";
 import { formAction } from "../actions";
 import IncrementButton from "./IncrementButton";
 
 export default function RscPage() {
-
-
+  const [state, formDispatch] = useFormState(formAction, 0);
 
   return (
     <>
       <div className="flex  items-center justify-center mt-10">
-        <form action={formAction}>
-        <IncrementButton />
+        <form action={formDispatch}>
+          <IncrementButton />
         </form>
+        <span>{state}</span>
       </div>
     </>
   );
